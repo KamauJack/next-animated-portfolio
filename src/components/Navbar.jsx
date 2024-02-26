bff17e207ad8a3dd518ec455422b490d3de40b7c
KamauJack <86573829+KamauJack@users.noreply.github.com>
2024-02-26 19:58:43 +0300

    mobile menu start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 
+const links = [
+  {url: "/", title:"Home"},
+  {url: "/about", title:"About"},
+  {url: "/portfolio", title:"Portfolio"},
+  {url: "/contact", title:"Contact"},
+]
 const Navbar = () => {
+
+
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* LOGO */}
@@ -16,7 +24,21 @@ const Navbar = () => {
         </Link>
       </div>
       {/* RESPONSIVE MENU */}
-      <div className=""></div>
+      <div className="">
+          {/* MENU BUTTON */}
+        <button className="w-10 h-8 flex flex-col justify-between">
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+        </button>
+           {/* MENU LISt */}
+           <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col">
+            {links.map((link)=>(
+            <Link href={link.url} key={link.title}>{link.title}</Link>)
+              
+            )}
+           </div>
+      </div>
     </div>
   );
 };
