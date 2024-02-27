921df891c211e284e8baee6be1345b6d2a2f362c
KamauJack <86573829+KamauJack@users.noreply.github.com>
2024-02-27 20:57:07 +0300

    mobile menu completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,36 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/*Image Container*/}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero.png" fill alt="hero" className="object-contain" />
+      </div>
+      {/*TEXT Container*/}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/*Title*/}
+        <h1 className="text-4xl md:text-6xl font-bol">
+          Crafting Digital Experiences, Designing Tomorrow
+        </h1>
+        {/*Desc*/}
+        <p className="md:text-xl">
+          Welcome to my digital canvas where innovation and creativity converge.
+          With a keen eye for aesthetics and a mastery of code, my portofolio
+          showcases a diverse collection of projects that reflect my commitment
+          to excellence
+        </p>
+        <div className="flex w-full gap-4">
+          <button className="p-1 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-1 rounded-lg ring-1 ring-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
