7ff1c8cbc65d0477a9caaeedc884a17520c539b5
Kamau Jack <emarketingharleys@gmail.com>
2024-03-03 18:10:51 +0300

    contact start

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+"use client";
+import { motion } from "framer-motion";
 const ContactPage = () => {
-  return <div>page</div>;
+  const text = "Say Hello";
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/*TEXT CONTAINER*/}
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <motion.div>
+            {text.split("").map((letter, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{ duration: 3, repeat: Infinity }}
+              >
+                {letter}
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+        {/*FORM CONTAINER*/}
+        <form className="h-1/2 lg:h-full lg:w-1/2"></form>
+      </div>
+    </motion.div>
+  );
 };
 
 export default ContactPage;
